1c3e6bbc778a21a83ae222701ff038a474917747
Yohann Legrand <yohann.legrand-ext@believe.com>
2022-06-09 12:24:13 +0200

    add flags to shoe cards

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -24,18 +24,22 @@ const ShoeCard = ({
   // on-sale. In theory, it is possible for a shoe to be
   // both on-sale and new-release, but in this case, `on-sale`
   // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
-    ? 'on-sale'
-    : isNewShoe(releaseDate)
-      ? 'new-release'
-      : 'default'
+  const variant =
+    typeof salePrice === "number"
+      ? "on-sale"
+      : isNewShoe(releaseDate)
+      ? "new-release"
+      : "default"
 
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === "new-release" && (
+            <NewReleaseLabel>Just Released!</NewReleaseLabel>
+          )}
+          {variant === "on-sale" && <NewSaleLabel>Sale</NewSaleLabel>}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
@@ -44,6 +48,7 @@ const ShoeCard = ({
         </Row>
         <Row>
           <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {!!salePrice && <SalePrice>{formatPrice(salePrice)}</SalePrice>}
         </Row>
       </Wrapper>
     </Link>
@@ -66,8 +71,29 @@ const Image = styled.img`
   width: 100%;
 `
 
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  border-radius: 2px;
+  padding: 7px 11px;
+  color: white;
+  font-weight: 700;
+  font-size: ${14 / 16}rem;
+`
+
+const NewReleaseLabel = styled(Flag)`
+  background-color: ${COLORS.secondary};
+`
+
+const NewSaleLabel = styled(Flag)`
+  background-color: ${COLORS.primary};
+`
+
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `
 
 const Name = styled.h3`
